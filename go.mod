fc08119d123b9fb31e5a404eb381bc205030de6f
kalifun <kalifun@163.com>
2021-03-26 19:57:39 +0800

    :tada: add orm

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.15
 
 require (
 	github.com/gin-gonic/gin v1.6.3
+	github.com/jinzhu/gorm v1.9.12 // indirect
 	github.com/spf13/viper v1.7.1
 )
--- main.go
+++ main.go
@@ -3,11 +3,25 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/kalifun/go-programming-tour-book-blog/global"
+	"github.com/kalifun/go-programming-tour-book-blog/internal/model"
 	"github.com/kalifun/go-programming-tour-book-blog/internal/routers"
 	"github.com/kalifun/go-programming-tour-book-blog/pkg/setting"
+	"log"
 	"net/http"
 	"time"
 )
+
+func init() {
+	err := setupSetting()
+	if err != nil {
+		log.Fatalf("init.setupSetting err: %v", err)
+	}
+
+	err = setupDBEngine()
+	if err != nil {
+		log.Fatalf("init.setupDBEngine err: %v", err)
+	}
+}
 
 func setupSetting() error {
 	newSetting, err := setting.NewSetting()
@@ -28,6 +42,16 @@ func setupSetting() error {
 	}
 	global.ServerSetting.ReadTimeout *= time.Second
 	global.ServerSetting.WriteTimeout *= time.Second
+	return nil
+}
+
+func setupDBEngine() error {
+	var err error
+	global.DBEngine, err = model.NewDBEngine(global.DatabaseSetting)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
